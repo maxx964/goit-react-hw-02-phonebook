e7f266761f94e99637ba56e8ec4aeda2a4df2a46
maxx964 <127957496+maxx964@users.noreply.github.com>
2023-10-06 18:59:42 +0300

    refactoring completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,50 +4,39 @@ import { nanoid } from 'nanoid';
 import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
-class App extends Component {
+
+
+  class App extends Component {
   state = {
-  contacts: [],
-  filter: '',
-  };
-  
-  constructor() {
-    super();
-    this.state = {
-      contacts: [
+    contacts: [
       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-      ],
-       filter: '',
-       name: '',
-       number: ''
+    ],
+    filter: '',
   };
-  };
- 
+
+  
   handleChange = (e) => {
     const { name, value } = e.target;
     this.setState({ [name]: value });
   };
 
-  handleSubmit = (e) => {
-    e.preventDefault();
-    const { name, contacts } = this.state;
-    if (contacts.some((contact) => contact.name === name)) {
+  handleSubmit = ({ name,number}) => {
+    if (this.state.contacts.some((contact) => contact.name === name)) {
       alert(`"${name}" is already in the list`);
       return;
   }
 const newContact = {
   id: nanoid(),
   name,
-  number: this.state.number,
+  number,
 };
 
 
   this.setState((prevState) => ({
     contacts: [...prevState.contacts, newContact],
-    name: '',
-    number: '',
   }));
 };
 
@@ -63,20 +52,15 @@ const newContact = {
   };
 
   render() {
-    return (
+    const { contacts, filter } = this.state;
+    return(
       <div>
-        <ContactForm
-          name={this.state.name}
-          number={this.state.number}
-          onChange={this.handleChange}
-          onSubmit={this.handleSubmit}
-        />
-       
-        <Filter value={this.state.filter} onChange={this.handleFilterChange} />
+        <ContactForm onSubmit={this.handleSubmit}/>
+        <Filter value={filter} onChange={this.handleFilterChange} />
         <ContactList
-          contacts={this.state.contacts}
+          contacts={contacts}
           onDeleteContact={this.handleDeleteContact}
-           filter={this.state.filter}
+          filter={filter}
         />
       </div>
     );
